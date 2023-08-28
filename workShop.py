57d9e34f2dfaf9b3469e5f07022287a429e64543
HelieFourgiotis <fourgiotishelie@gmail.com>
2023-08-28 17:04:10 -0300

    Enviando primeira atividade

---
+++ workShop.py
@@ -0,0 +1,7 @@
+nume1 = int(input("Digite um número: "))
+sucessor = 1 + nume1
+antecessor = nume1 - 1
+print(f"O antecessor do número {nume1} é {antecessor} e o sucessor é {sucessor}")
+print(type(nume1))
+print(type(antecessor))
+print(type(sucessor))
